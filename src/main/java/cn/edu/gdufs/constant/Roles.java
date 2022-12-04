c26ce033ac53e561dd282727514f5f18e38536a8
qt_ysp@163.com <qt_ysp@163.com>
2022-12-04 23:12:01 +0800

    feat: 新增权限常量类
    - pom.xml 添加 validation 和 swagger 依赖

---
+++ src/main/java/cn/edu/gdufs/constant/Roles.java
@@ -0,0 +1,13 @@
+package cn.edu.gdufs.constant;
+
+/**
+ * Description: 权限常量类
+ * Author: 严仕鹏
+ * Date: 2022/12/4
+ */
+public class Roles {
+    private Roles() {}
+
+    public static final int ROLE_SUPER_ADMIN = 1;   // 超级管理员权限
+    public static final int ROLE_NORMAL_ADMIN = 2;  // 普通管理员权限
+}
