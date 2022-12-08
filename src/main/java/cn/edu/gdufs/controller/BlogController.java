bb9ce717016baf4d39706a08cb34c1e01f09269d
qt_csy@163.com <qt_csy@163.com>
2022-12-08 14:02:29 +0800

    feat: 重命名文章实体类部分字段
    - 重命名文章创建用户id属性为 createUserId
    - 重命名文章最后修改用户id属性为 updateUserId
    - 修改数据库中字段名
    - 添加“新增文章接口”

--- src/main/java/cn/edu/gdufs/controller/BlogController.java
+++ src/main/java/cn/edu/gdufs/controller/BlogController.java
@@ -1,6 +1,11 @@
 package cn.edu.gdufs.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import cn.edu.gdufs.config.interceptor.RequiredPermission;
+import cn.edu.gdufs.constant.RoleConstant;
+import cn.edu.gdufs.pojo.Blog;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
  * Description:
@@ -8,5 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
  * Date: 2022/12/3
  */
 @RestController
+@RequestMapping("/blog")
 public class BlogController extends BaseController {
+
+    /**
+     * 新增文章
+     */
+    @PostMapping
+    @RequiredPermission({RoleConstant.ROLE_SUPER_ADMIN, RoleConstant.ROLE_NORMAL_ADMIN})
+    public void insertBlog(@RequestBody @Valid Blog blog) {
+
+    }
 }
